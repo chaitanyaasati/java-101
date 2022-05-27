b096b8e24d5c85ea049044dac646c43450acc787
Chaitanya Asati <chaitanya.asati1@gmail.com>
2022-05-27 22:16:58 +0530

    blank commit

--- A1Java/src/com/chaitanya/java/a1/sr_001/Main.java
+++ A1Java/src/com/chaitanya/java/a1/sr_001/Main.java
@@ -3,6 +3,6 @@ package com.chaitanya.java.a1.sr_001;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("i am here");
     }
 }
